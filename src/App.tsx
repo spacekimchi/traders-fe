f8e7be4d26ab9e85ffd224da6422ff82334f18a5
spacekimchis <spacekimchis@gmail.com>
2023-02-15 17:40:15 -0800

    feat: calendar component and layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Calendar from './components/Calendar';
+import './stylesheets/app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="app-container">
+			<div className="left-panel">
+				<Calendar />
+			</div>
+			<div className="main-content">
+				main content
+			</div>
+		</div>
+	);
 }
 
 export default App;
