bc6774cb0343e7ff3a3cc05752c438677fe41843
spacekimchis <spacekimchis@gmail.com>
2023-05-04 10:08:49 -0700

    browser buttons
    wip
    
    browser back button and clicking days

--- src/Month/Month.tsx
+++ src/Month/Month.tsx
@@ -5,11 +5,26 @@ import { MONTHS, DAYS } from '../utils/constants';
 import DollarVal from '../DollarVal/DollarVal';
 import { useCalendarContext } from '../CalendarView/CalendarView';
 import { SUNDAY, SATURDAY } from '../utils/constants';
+import { useState, useEffect } from 'react';
+import { useParams, Outlet, useNavigate } from 'react-router-dom';
 
 export default function Month() {
-    const { year, month, trades, accounts } = useCalendarContext();
+    // First setting..
+    let { year, month, trades, accounts, setMonth, setDay } = useCalendarContext();
+    const navigate = useNavigate();
+
+    // This is mainly for when user presses the browser's back button
+    const monthParam = Number(useParams()['month']);
+    if (monthParam) {
+        month = monthParam - 1;
+    }
+
     const simAccount = accounts.find((account: Account) => account.sim);
     let tradesByDay = groupTradesByDay(trades, simAccount);
+
+    useEffect(() => {
+        setMonth(month);
+    });
 
     function createCalendar() {
         const firstOfMonth = new Date(year, month, 1);
@@ -49,7 +64,7 @@ export default function Month() {
         days.forEach((day: Date, idx: number) => {
             let d = dateToExcel(day);
             const dayHtml = (
-                <div key={idx} className="day">
+                <div key={idx} className="day" onClick={(e) => {handleDayClick(e, day.getMonth(), day.getDate())}}>
                     <div>{day.getDate()}</div>
                     <div>{DAYS[day.getDay()]}</div>
                     <div>{d in tradesByDay ? <DollarVal val={getPnl(tradesByDay[d].trades).toFixed(2)} /> : ""}</div>
@@ -62,6 +77,12 @@ export default function Month() {
             }
         });
         return weeks;
+    }
+
+    function handleDayClick(e: any, m: number, val: number) {
+        e.preventDefault();
+        setDay(val);
+        navigate(`/calendar/${year}/${m+1}/${val}`);
     }
 
     const weeks = createCalendar().map((week: any, idx: number) => {
